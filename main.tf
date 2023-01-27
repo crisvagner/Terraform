259a76e9431fe441af083c1f2c1c4e00d909ad25
eokrizz <crisvagnersd@gmail.com>
2023-01-27 19:21:01 -0300

    utilizando Terraform no Provider Digital Ocean

--- main.tf
+++ main.tf
@@ -47,3 +47,11 @@ variable "region" {
     default = ""
 }
 
+output "jenkins_id" {
+  value  = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
